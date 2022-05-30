5da54115a259823a7afc166393dc12511838a602
Askhab Magomedov <asmag86@gmail.com>
2022-05-30 16:08:43 +0300

    finalizing 12 lesson

--- 12/script.js
+++ 12/script.js
@@ -1,11 +1,27 @@
-const arr = [1, 2, 3];
+const arr = ["a", "b", "c"];
 
-console.log(arr[1]);
+arr[10] = "3456";
+
+const arrObj = {
+  0: "a",
+  "1": "b",
+  2: "c",
+  abc: {
+    df: [{}, {}],
+    def: {
+
+    }
+  }
+};
+const b = "b";
+
+//arrObj.b = "1234";
+arrObj[b] = [];
+
+console.log(arrObj["b"]);
 
 // const obj = {a: 1, b: 2};
 const obj = {
   'Anna': 500,
   'Alice': 800
 };
-
-['Anna', 500, 'Alice', 800]
