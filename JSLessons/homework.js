8b8184af9840335293eea3696b2284a2c9f2fb4a
Askhab Magomedov <asmag86@gmail.com>
2022-06-01 16:17:08 +0300

    end homework #23.3

--- JSLessons/homework.js
+++ JSLessons/homework.js
@@ -1,47 +1,14 @@
-// Место для первой задачи
-function firstTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const arr = [3, 5, 8, 16, 20, 23, 50];
-    const result = [];
-
-    // Пишем решение вот тут
-    for(let i = 0; i <= arr.length; i++) {
-        result[i] = arr[i];
+const lines = 5;
+let result = '';
+// Проверяется именно переменная result, формируйте строку в ней
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
     }
-    
-    // Не трогаем
-    return result;
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
 }
 
-// Место для второй задачи
-function secondTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-
-    // Пишем решение вот тут
-    for(let i = 0; i <= data.length; i++) {
-        if(typeof(data[i]) === "string") {
-            data[i] += " - done";
-        } else {
-            data[i] *= 2;
-        }
-    }
-    
-    // Не трогаем
-    return data;
-}
-
-// Место для третьей задачи
-function thirdTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-    const result = [];
-
-    // Пишем решение вот тут
-    for (let i = 1; i <= data.length; i++) {
-        result[i - 1] = data[data.length - i]
-    }
-    
-    // Не трогаем
-    return result;
-}
+console.log(result)
