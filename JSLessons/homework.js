2dfd7f9c00c8ad07fa0f0a78744664be6260f976
Askhab Magomedov <asmag86@gmail.com>
2022-06-03 16:46:19 +0300

    in the middle of homework #29.3

--- JSLessons/homework.js
+++ JSLessons/homework.js
@@ -1,33 +1,29 @@
-// Место для первой задачи
-function getTimeFromMinutes(number) {
-    if (number >= 0 && number < 60) {
-        return console.log(`Это 0 часов и ${number} минут`);
-    } else if (number >= 60) {
-        return console.log(`Это ${Math.floor(number / 60)} часов и ${number % 60} минут`);
+function fib(num) {
+    let str = "";
+    let sum = 0;
+    let change = 0;
+    if (!num || typeof(num) !== "number") {
+        return console.log(str);
     } else {
-        return console.log(`Не верно указано число минут`);
-    }
-}
-
-getTimeFromMinutes(256);
-
-// Место для второй задачи
-function findMaxNumber(numOne, numTwo, numThree, numFour) {
-    if (!numOne || !numTwo || !numThree || !numFour || typeof(numOne) !== "number" || typeof(numTwo) !== "number" || typeof(numThree) !== "number" || typeof(numFour) !== "number") {
-        return console.log(0);
-    } else {
-        let bigNum = 0;
-        let numArr = [numOne, numTwo, numThree, numFour];
-
-        for (let i = 0; i < numArr.length; i++) {
-            if (numArr[i] > bigNum) {
-                bigNum = numArr[i];
+        for (let i = 0; i < num; i++) {
+            if (typeof(num) !== "number" || !num) {
+                console.log(str);
             } else {
-                continue;
+                if (i === 0) {
+                    str += `${sum} `;
+                    change += 1;
+                } else {
+                    sum = change;
+                    str += `${sum} `;
+                    sum += change;
+                }
             }
         }
-        return console.log(bigNum);
+        return console.log(str);
     }
 }
 
-findMaxNumber(1, 5, 6.6, 11);
+fib(4);
+fib(7);
+fib("7");
+fib(0);
