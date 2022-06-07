17503f026b46da5a874a69c86541aeaf5f039b38
Askhab Magomedov <asmag86@gmail.com>
2022-06-07 17:09:52 +0300

    end homework #10

--- JSLessons/homework.js
+++ JSLessons/homework.js
@@ -1,24 +1,46 @@
-function fib(num) {
-    let str = "";
-    let sum = 0;
-    let change = 1;
-    if (!num || typeof(num) !== "number" || !Number.isInteger(num)) {
-        return console.log(str);
-    } else {
-        for (let i = 1; i <= num; i++) {
-            if (i === num) {
-                str += `${sum}`;
-            } else {
-                str += `${sum} `;
-                [sum, change] = [change, sum];
-                change += sum;
-            }
-        }
-        return console.log(str);
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
     }
+};
+
+function showExperience(plan) {
+    let {skills: {exp: experience}} = plan;
+    console.log(`${experience}`);
 }
 
-fib(4);
-fib(7);
-fib("7");
-fib(0);
+showExperience(personalPlanPeter);
+
+function showProgrammingLangs(plan) {
+    let str = ``;
+    let {skills: {programmingLangs}} = plan;
+    if (Object.entries(programmingLangs)) {
+        for (let lang in programmingLangs) {
+            str += `Язык ${lang} изучен на ${programmingLangs[lang]}\n`
+        }
+    }
+    console.log(str);
+}
+
+showProgrammingLangs(personalPlanPeter);
+
+personalPlanPeter.showAgeAndLangs = function(obj) {
+    let {age} = obj;
+    let {skills: {languages}} = obj;
+    let str = `Мне ${age} и я владею языками: `;
+    if (Object.entries(languages)) {
+        for (let lang in languages) {
+            str += `${languages[lang].toUpperCase()} `;
+        }
+    }
+    console.log(str);
+};
+
+personalPlanPeter.showAgeAndLangs(personalPlanPeter);
