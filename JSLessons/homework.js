be93a0c88eadd84a0106bfb2dc4a74e68b1051d8
Askhab Magomedov <asmag86@gmail.com>
2022-06-14 14:17:36 +0300

    end homework #14

--- JSLessons/homework.js
+++ JSLessons/homework.js
@@ -1,43 +1,21 @@
-const shoppingMallData = {
-    shops: [
-        {
-            width: 10,
-            length: 5
-        },
-        {
-            width: 15,
-            length: 7
-        },
-        {
-            width: 20,
-            length: 5
-        },
-        {
-            width: 8,
-            length: 10
-        }
-    ],
-    height: 5,
-    moneyPer1m3: 30,
-    budget: 50000
-}
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
 
-function isBudgetEnough(data) {
-    let {shops} = data;
-    let mallSquare = 0;
-    let mallVolume = 0;
-
-    for (let i of shops) {
-        mallSquare += i.width * i.length;
-    }
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    const a = [], b = [], c = [], rest = [];
     
-    mallVolume += mallSquare * data.height;
-
-    if (mallVolume * data.moneyPer1m3 > data.budget) {
-        return console.log(`Бюджета недостаточно`);
-    } else {
-        return console.log(`Бюджета достаточно`);
+    for (let i = 0; i < arr.length; i++) {
+        if (i < 3) {
+            a.push(arr[i]);
+        } else if (i < 6) {
+            b.push(arr[i]);
+        } else if (i < 9) {
+            c.push(arr[i]);
+        } else {
+            rest.push(arr[i]);
+        }
     }
+    return console.log([a, b, c, `Оставшиеся студенты: ${rest.length === 0 ? "-" : rest.join(", ")}`]);
 }
 
-isBudgetEnough(shoppingMallData);
+sortStudentsByGroups(students);
