96a4473e08fbb1d6562921f4c2e8e49ae1b3def6
Askhab Magomedov <asmag86@gmail.com>
2022-06-15 15:27:15 +0300

    end lesson #45

---
+++ preview/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const btns = document.querySelectorAll(`button`),
+      overlay = document.querySelector(`.overlay`);
+
+// btn.onclick = function() {
+//   alert(`Click`);
+// };
+
+// btn.onclick = function() {
+//   alert(`Second click`);
+// };
+
+// let i = 0;
+const deleteElement = (event) => {
+  console.log(event.currentTarget);
+  console.log(event.type);
+  // i++;
+  // if (i === 1) {
+  //   btn.removeEventListener(`click`, deleteElement);
+  // }
+};
+
+// btn.addEventListener(`click`, deleteElement);
+// overlay.addEventListener(`click`, deleteElement);
+
+btns.forEach(btn => {
+  btn.addEventListener(`click`, deleteElement, {once: true});
+});
+
+const link = document.querySelector(`a`);
+
+link.addEventListener(`click`, function(event) {
+  event.preventDefault();
+  console.log(event.target);
+});
