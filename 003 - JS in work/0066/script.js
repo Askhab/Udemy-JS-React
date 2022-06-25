e601da91f15526a1b2e6760b4fbc9f7e5e0f7485
Askhab <asmag86@gmail.com>
2022-06-25 23:56:31 +0300

    end lesson #66

---
+++ 003 - JS in work/0066/script.js
@@ -0,0 +1,36 @@
+// function func() {
+//   window.smth = `string`;
+// }
+
+// const someRes = getData ();
+// const node = document.querySelector(`.class`);
+
+// setInterval(function() {
+//   if (node) {
+//     node.innerHTML = someRes;
+//   }
+// }, 1000);
+
+// function outer() {
+//   const potentialHugeArray = [];
+//   return function inner() {
+//     potentialHugeArray.push(`Hello`);
+//     console.log(`Hello!`);
+//   };
+// }
+
+// const sayHello = outer();
+
+function createElement() {
+  const div = document.createElement(`div`);
+  div.id = `test`;
+  document.body.append(testDiv);
+}
+
+createElement();
+
+function deleteElement() {
+  document.body.removeChild(document.getElementById(`test`));
+}
+
+deleteElement();
